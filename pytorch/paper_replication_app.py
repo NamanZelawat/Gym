12b8db0e3a245e2fed57b733f8a8efdaa0de5d78
NamanZelawat <namanzelawat@gmail.com>
2025-12-10 12:32:32 +0900

    Setup streamlit app

---
+++ pytorch/paper_replication_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Vision Transformer Paper Replication")
+
+st.write("This is a replication of the ViT paper using PyTorch.")
+
+st.write("## Upload an image")
+st.write("Upload an image to classify from pizza, steak, or sushi. Only 3 categories are supported.")
+uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg'])
+
+st.write("## Prediction")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', width=250)
+    st.write("## Result")
+    st.write(f"The image is a pizza, steak, or sushi.")
+
+
